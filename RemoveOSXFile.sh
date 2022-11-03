3ad79302c3e7ff3577ae3675d62b1f6872faf485
mikael <mikael.fangel@pm.me>
2022-11-03 13:35:26 +0100

    added ds store to be removed

--- RemoveOSXFile.sh
+++ RemoveOSXFile.sh
@@ -3,10 +3,11 @@ while getopts n flag
 
 do
     case "$flag" in
-        n) find . -type d -name '__MACOSX' ;;
+        n) find . -type d -name '__MACOSX' ; find . -type f -name '.DS_STORE' ;;
     esac
 done
 
 if [ $OPTIND -eq 1 ] ; then
     find . -type d -name '__MACOSX' -exec rm -rf {} +
+    find . -type f -name '.DS_STORE' -exec rm -rf {} +
 fi
