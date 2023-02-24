3d72027df2d6a22f199b3e80910aaf141ee3b2a3
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-02-24 19:31:46 +0100

    initial commit for gitpolite

---
+++ gitpolite.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+usualsuspects=~/.gitco_suspects
+suspects="$PWD/.suspects"
+summary=$(gum input --placeholder "Summary of this change")
+description=$(gum write --placeholder "Details of this change (CTRL+D to finish)" --show-line-numbers)
+convicts=$(cat $usualsuspects $suspects 2>/dev/null | grep "\S" | sort | uniq | gum choose --no-limit)
+
+gum confirm "Commit the message?" && git commit -m "$summary
+
+$description
+
+$convicts"
