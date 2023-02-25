e2e64eca8ab412028308218596874ab53a7b75b5
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-02-25 09:41:24 +0100

    added options -am to work more as commit

--- gitpolite.sh
+++ gitpolite.sh
@@ -1,13 +1,34 @@
 #!/bin/env bash
 
-if [ "$1" = "polite" ]; then
+findconvicts() {
     usualsuspects=~/.gitco_suspects
     suspects="$PWD/.suspects"
-    summary=$(gum input --placeholder "Summary of this change")
-    description=$(gum write --placeholder "Details of this change (CTRL+D to finish)" --show-line-numbers)
     convicts=$(cat $usualsuspects $suspects 2>/dev/null | grep "\S" | sort | uniq | gum choose --no-limit)
+}
 
-    gum confirm "Commit the message?" && git commit -m "$summary
+if [ "$1" = "polite" ]; then
+        # Remove polite from the argument list
+        shift
+
+        # Check provided arguments
+        while getopts "am:" flag; do
+            case "$flag" in
+                a) git add --all ;;
+                m) summary="$OPTARG" ;;
+            esac
+        done
+
+        # If no summary has been provied assume interactive mode
+        if [ -z "$summary" ]; then
+            summary=$(gum input --placeholder "Summary of this change")
+            description=$(gum write --placeholder "Details of this change (CTRL+D to finish)" --show-line-numbers)
+        fi
+
+        # Always suggest adding co-authors
+        findconvicts
+ 
+        # Confirm the commit
+        gum confirm "Commit the changes?" && git commit -m "$summary
 
 $description
 
