87c3a9d04699b151c5d08547f77cbece1f923590
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-05-05 10:01:29 +0200

    chore: refactor code and add check for path existing

--- rmosxf.sh
+++ rmosxf.sh
@@ -1,36 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 [[ $OSTYPE == 'darwin'* ]] && echo "Detected MACOSX... Exiting" && exit 0
 
 path=$HOME
 do_clean=1
 
-function clean() {
-    if [[ $do_clean -eq 1 ]] ; then
-        find "$path" -type d -name '__MACOSX' -exec rm -rfv {} + &
-        find "$path" -type f \( -name '.DS_STORE' -o -name '.DS_Store' \) -exec rm -rfv {} +
-    else
-        find "$path" -type d -name '__MACOSX' &
-        find "$path" -type f \( -name '.DS_STORE' -o -name '.DS_Store' \)
-    fi
-}
-
-while getopts p:nh flag
-
-do
-    case "$flag" in
-        p)
-        echo "Your path is set to $OPTARG"
-        path=$OPTARG ;;
-        n)
-        echo "Running a dry run..."
-        do_clean=0 ;;
-        *)
-        echo \
-    'Usage:
+function show_help() {
+    echo 'Usage:
     -n      dry-run to print what files will be removed
     -p      set starting point in the path
     -h      show help'
+    exit 0
+}
+
+function clean() {
+    if [[ ! -d "$path" ]]; then
+        echo "Error: Path '$path' does not exist"
+        exit 1
+    fi
+
+    if [[ $do_clean -eq 1 ]]; then
+        echo "Removing files..."
+        find "$path" -type d -name '__MACOSX' -exec rm -rf {} \;
+        find "$path" -type f -name '.DS_Store' -exec rm -f {} \;
+    else
+        echo "Dry run - would remove these files:"
+        find "$path" -type d -name '__MACOSX'
+        find "$path" -type f -name '.DS_Store'
+    fi
+}
+
+while getopts "p:nh" flag; do
+    case "$flag" in
+        p)
+            echo "Your path is set to $OPTARG"
+            path=$OPTARG 
+            ;;
+        n)
+            echo "Running a dry run..."
+            do_clean=0 
+            ;;
+        h)
+            show_help
+            ;;
+        *)
+            show_help
+            ;;
     esac
 done
 
